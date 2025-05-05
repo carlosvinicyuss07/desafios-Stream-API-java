929a1a92510fdc7cffb1c48f5ecb3499125b089f
Carlos Vinícyus <carlosvinicyuss@gmail.com>
2025-05-04 23:47:47 -0300

    Resolução Desafios - do 01 ao 05

---
+++ src/desafios/Desafio05.java
@@ -0,0 +1,18 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio05 {
+
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        Integer soma = numeros.stream().filter(n -> n > 5).reduce(0, Integer::sum);
+
+        Long media = soma / (numeros.stream().filter(n -> n > 5).count());
+
+        System.out.println(media);
+    }
+}
